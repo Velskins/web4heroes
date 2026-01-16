ec2190db2d99d192598696e046fbe35ca8668a45
Jeff Benard <pro.jeffbenard@gmail.com>
2026-01-16 11:08:59 +0100

    add views auth for collaborator

--- views/auth/login.php
+++ views/auth/login.php
@@ -3,17 +3,7 @@
 </h1>
 
 <form action="/login" method="POST">
-    <div>
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required>
-    </div>
 
-    <div>
-        <label for="pwd">Mot de passe :</label>
-        <input type="password" id="pwd" name="pwd" required>
-    </div>
-
-    <button type="submit">Se connecter</button>
 </form>
 
 <p>Pas encore de compte ? <a href="/register">S'inscrire ici</a></p>
--- views/auth/register.php
+++ views/auth/register.php
@@ -9,43 +9,5 @@
 <?php endif; ?>
 
 <form action="/register" method="POST">
-    <div>
-        <label for="lastname">Nom :</label>
-        <input type="text" id="lastname" name="lastname" required>
-    </div>
-    <div>
-        <label for="firstname">Prénom :</label>
-        <input type="text" id="firstname" name="firstname" required>
-    </div>
 
-    <div>
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required>
-    </div>
-    <div>
-        <label for="pwd">Mot de passe :</label>
-        <input type="password" id="pwd" name="pwd" required>
-    </div>
-
-    <div>
-        <label for="birthdate">Date de naissance :</label>
-        <input type="date" id="birthdate" name="birthdate" required>
-    </div>
-
-    <div>
-        <label for="gender">Genre :</label>
-        <select name="gender" id="gender">
-            <option value="Male">Homme</option>
-            <option value="Femelle">Femme</option>
-            <option value="Other">Autre</option>
-        </select>
-    </div>
-
-    <div style="margin: 15px 0;">
-        <input type="checkbox" id="is_hero" name="is_hero" value="1">
-        <label for="is_hero"><strong>Je souhaite m'inscrire en tant que Super-Héros</strong> (soumis à
-            validation)</label>
-    </div>
-
-    <button type="submit">Créer mon compte</button>
 </form>
